d12de82dea0745b5590353c1fbd2ffe9deaabde8
mateuszkaczmarczyk <mateusz.kaczmarczyk@tylko.com>
2025-05-20 15:37:42 +0200

    next versions of textures

--- compress_textures.sh
+++ compress_textures.sh
@@ -7,7 +7,7 @@ mkdir -p ktx2_output
 for file in sofa_textures/*/*.png; do
   if [[ -f "$file" ]]; then
     base=$(basename "$file" .png)
-    echo "🔄 Compressing $file → ktx2_output/${base}.ktx2"
+    echo "🔄 Compressing $file → ktx2_output/${base}_v2.ktx2"
     toktx --encode etc1s --clevel 4 --qlevel 128 --genmipmap --2d --assign_oetf srgb \
       "ktx2_output/${base}.ktx2" "$file"
   fi
